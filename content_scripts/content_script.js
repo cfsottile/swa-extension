867fe6d31ba4290d37caac7dbef6eb1bd3de6fe0
Cristian Sottile <cfsottile@gmail.com>
2017-08-31 16:36:25 -0300

    WIP content script

--- content_scripts/content_script.js
+++ content_scripts/content_script.js
@@ -1,47 +1,98 @@
-console.log("loading content");
+function doCurrentTab(fn) {
+    browser.tabs.query({
+        currentWindow: true,
+        active: true
+    }).then((tabs) => {
+        tabs.forEach(fn);
+    });
+}
+
+var loaded = false;
+var originalPadding = document.getElementsByTagName("body")[0].style["padding-left"];
+
+function onClickedBrowserAction() {
+    if (loaded) { unloadSidebar(); }
+    else { loadSidebar(); }
+}
+
+function loadSidebar() {
+    document.getElementsByTagName("body")[0].style["padding-left"] = "350px";
+    loaded = true;
+    document.getElementsByTagName("body")[0].appendChild(prepareSidebar());
+}
+
+function prepareSidebar() {
+    var sidebar = document.createElement("div");
+    sidebar.id = '__swa_sidebar';
+    setStyles(sidebar);
+    let tmpdoc = (new DOMParser()).parseFromString(sidebarHtml(), 'text/html');
+    setScripts(tmpdoc);
+    sidebar.appendChild(tmpdoc.body.firstChild);
+    return sidebar;
+}
+
+function setStyles(sidebar) {
+    sidebar.style.position = 'fixed';
+    sidebar.style.left = '0px';
+    sidebar.style.top = '0px';
+    sidebar.style["z-index"] = 9999;
+    sidebar.style.width = '350px';
+    sidebar.style.height = '100%';
+    sidebar.style["background-color"] = 'white';
+}
+
+function setScripts(doc) {
+    doc.getElementById("selector-button").onclick = select;
+}
+
+function unloadSidebar() {
+    document.getElementsByTagName("body")[0].style["padding-left"] = originalPadding;
+    loaded = false;
+    var sidebar = document.getElementById("__swa_sidebar");
+    sidebar.parentElement.removeChild(sidebar);
+}
 
 browser.runtime.onMessage.addListener(request => {
     console.log(request);
-    return Promise.resolve({response: run(request.codop, request.args)});
+    // return Promise.resolve({response: run(request.codop, request.args)});
+    run(request);
 });
 
-function run(codop, args) {
-    return ops[codop](args);
+function run(request) {
+    return ops[request.codop](request.args);
 }
 
 var ops = {
-    "augment": preAugment,
-    "select": select
+    "browser-action": onClickedBrowserAction,
 }
 
 function select() {
+    console.log("select");
     // habilitar el highlight
-    document.addEventListener("click", getXpath);
+    document.addEventListener("dblclick", getXpath);
 }
 
 function getXpath(mouseEvent) {
-    document.removeEventListener("click", getXpath);
+    document.removeEventListener("dblclick", getXpath);
     // deshabilitar el hightlight
-    return createXPathFromElement(mouseEvent.target);
+    document.getElementById("selector-xpath-label").textContent = createXPathFromElement(mouseEvent.target);
 }
 
 function preAugment(args) {
-    let selection = args["selector-xpath"];
-    if (!args["selector-many"]) {
+    let selection = document.getElementById("selector-xpath").value;
+    if (!document.getElementById("selector-many").value) {
         selection = [selection];
     }
     let select = gSelect(() => selection);
     
-    let extractionStrategy = args["extractor-select"];
+    let extractionStrategy = document.getElementById("extractor-select").value;
     let extract = gExtract(extractionStrategies[extractionStrategy]);
     
-    let templateQuery = args["query"];
+    let templateQuery = document.getElementById("query").value;
     let fetch = gFetch(templateQuery, autoParser(templateQuery));
     
     // ...
     
-    console.log(args);
-
     // augment(select, extract, fetch, build, inject);
 
     return true;
@@ -61,6 +112,79 @@ function autoParser(templateQuery) {
             .forEach((e) => result[e] = data.results.bindings[0][e].value);
         return result;
     }
+}
+
+function sidebarHtml() {
+    return `
+    <div>
+    <h3>Semantic Web Augmentation</h3>
+    
+        <hr>
+    
+        <div id="selector" style="margin:10px">
+          <h3>Selection</h3>
+          <div style="margin:10px">
+            <input type="button" id="selector-button" value="Select structure">
+            <input type="checkbox" id="selector-many"> <label>Many</label>
+          </div>
+          <div style="margin:10px">
+            
+            <label id="selector-xpath-label"></label>
+          </div>
+        </div>
+    
+        <hr>
+    
+        <div id="extractor" style="margin:10px">
+          <h3>Extraction</h3>
+          <div style="margin:10px">
+          <label>Data extraction strategy:</label>
+          <select id="extractor-select">
+            <option>text content</option>
+            <option>href</option>
+          </select>
+          </div>
+        </div>
+    
+        <hr>
+      
+        <div id="fetcher" style="margin:10px">
+          <h3>Querying</h3>
+          <div style="margin:10px">
+            <textarea id="query"></textarea>
+          </div>
+        </div>
+        
+        <hr>
+    
+        <div id="builder" style="margin:10px">
+          <h3>Building</h3>
+          <div style="margin:10px">
+            <textarea></textarea>
+          </div>
+        </div>
+        
+        <hr>
+    
+        <div id="injector" style="margin:10px">
+          <h3>Injection</h3>
+          <div style="margin:10px">
+            <input type="button" id="injector-button" value="Select node" onclick="select()">
+            <select>
+              <option>insert after</option>
+              <option>insert before</option>
+            </select>
+          </div>
+          <div style="margin:10px">
+            <input type="text" id="selector-xpath" disabled>
+          </div>      
+        </div>
+        
+        <hr>
+    
+        <input type="button" value="Augment" style="display: block; margin: 0 auto; margin-top: 30px">
+        </div>
+    `;
 }
 
 // code not mine
