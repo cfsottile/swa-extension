2c251239f4d9f5644b1a464e8c0d31ebde54e91a
Cristian Sottile <cfsottile@gmail.com>
2019-03-25 23:41:00 -0300

    Sidebar updated following new structure

--- sidebar-handler.js
+++ sidebar-handler.js
@@ -8,6 +8,9 @@ var originalPadding = document.getElementsByTagName("body")[0].style["padding-le
 
 var data = [];
 var amountOfEntries = 0;
+var stageHandler;
+var selectionType = SelectionEntrySingle;
+var injectionType = InjectionMap;
 
 function toogleSidebar() {
     if (loaded) { unloadSidebar(); }
@@ -28,6 +31,9 @@ function loadSidebar() {
 function prepareSidebar() {
     var sidebar = document.createElement("div");
     sidebar.id = '__swa_sidebar';
+    swaStyles = document.createElement("style");
+    document.head.appendChild(swaStyles);
+    stageHandler = new StageHandler(swaStyles);
     setStyles(sidebar);
     let tmpdoc = (new DOMParser()).parseFromString(sidebarHtml(), 'text/html');
     setScripts(tmpdoc);
@@ -50,9 +56,13 @@ function setScripts(doc) {
     doc.getElementById("selector-many").onchange = handleManySelections;
     doc.getElementById("builder-select").selectedIndex = -1;
     doc.getElementById("builder-select").onchange = adjustBuilderSelection;
+    doc.querySelector("#builder-select").value = "Map";
     doc.getElementById("injector-button").onclick = injectionListener(doc.getElementById("injector"), 1);
-    doc.getElementById("augment-button").onclick = preAugment;
+    doc.getElementById("try-button").onclick = preAugment;
+    // doc.getElementById("save-button").onclick = preAugment;
     doc.getElementById("vsb-button").onclick = openVSB;
+    doc.querySelectorAll(".next-button").forEach(e => e.onclick = () => stageHandler.next());
+    doc.querySelectorAll(".previous-button").forEach(e => e.onclick = () => stageHandler.previous());
     console.log("loaded")
 }
 
@@ -89,37 +99,43 @@ function getSelection(entryNumber) {
 
 function adjustBuilderSelection() {
     switch (document.getElementById("builder-select").value) {
-        case "N - N - N":
+        case "Map":
             renderBuilderInput(1);
-            setManyInjections(true);
+            preBuild = preBuildMap;
+            injectionType = InjectionMap;
+            // setManyInjections(true);
             break;
-        case "N - M - 1":
-            renderBuilderInput(3);
-            setManyInjections(false);
-            break;
-        case "N - M - N":
+        case "Converge":
             renderBuilderInput(2);
-            setManyInjections(true);
+            preBuild = preBuildConvergence;
+            injectionType = InjectionConvergence;
+            // setManyInjections(false);
             break;
-        case "N - N - 1":
-            renderBuilderInput(2);
-            setManyInjections(false);
-            break;
+        // case "N - M - 1":
+        //     renderBuilderInput(3);
+        //     setManyInjections(false);
+        //     break;
+        // case "N - M - N":
+        //     renderBuilderInput(2);
+        //     setManyInjections(true);
+        //     break;
     }
 }
 
 function renderBuilderInput(n) {
     let textAreasDiv = document.createElement("div");
+    textAreasDiv.id = "swa-builder-textarea";
     addTextAreas(textAreasDiv, n);
-    let builderDiv = document.getElementById("builder-div");
-    builderDiv.removeChild(builderDiv.lastChild);
-    builderDiv.appendChild(textAreasDiv);
+    let builderTextareaDiv = document.getElementById("swa-builder-textarea-div");
+    builderTextareaDiv.removeChild(builderTextareaDiv.querySelector("#swa-builder-textarea"));
+    builderTextareaDiv.appendChild(textAreasDiv);
 }
 
 function addTextAreas(textAreasDiv, n) {
     for (var i = 1; i <= n; i++) {
         let area = document.createElement("textarea");
         area.id = "building-" + i;
+        area.style = "margin-top: 2px; width: 304px; height: 91px";
         textAreasDiv.appendChild(area);
     }
     // console.log(textAreasDiv);
@@ -130,12 +146,14 @@ function setManyInjections(bool) {
 }
 
 function handleManySelections() {
+    selectionType = SelectionEntryMultiple;
     checked = document.getElementById("selector-many").checked;
     if (checked) {
-        updateManySelectionsFunc();
+        // updateManySelectionsFunc();
+        selectionType = SelectionEntryMultiple;
         addManySelectionsUI();
     } else {
-        rollbackManySelectionsFunc();
+        // rollbackManySelectionsFunc();
         removeManySelectionsUI();
     }
 }
@@ -144,17 +162,20 @@ function addManySelectionsUI() {
     let selectionEntries = Array.from(document.getElementById("selector-group").getElementsByClassName("selection-entry"));
     selectionEntries.forEach((se) => {
         se.querySelector(".select-element").value = "Select first element";
+        se.querySelector(".selection-value-label-1").textContent = "Value of first element";
         let entryNumber = selectionEntryNumber(se);
-        se.appendChild(stringToNode(divSelectionMany(entryNumber)));
+        se.querySelector(".selection-buttons").appendChild(stringToNode(buttonSelectionLast(entryNumber)));
+        se.querySelector(".selection-xpaths").appendChild(stringToNode(selectionXpathLast(entryNumber)));
+        se.querySelector(".selection-values").appendChild(stringToNode(valueLastSelection(entryNumber)));
         console.log(se, entryNumber);
         se.querySelector(`#select-many-last-${entryNumber}`).onclick =
             selectionNListener(se, `${entryNumber}-2`);
     });
     let injectorDiv = document.getElementById('injector').children[1];
-    injector.appendChild(stringToNode(divInjectionMany()));
+    injector.querySelector(".injection-buttons").appendChild(stringToNode(buttonInjectionLast()));
+    injector.querySelector(".injection-xpaths").appendChild(stringToNode(injectionXpathLast()));
     injector.querySelector("#injector-button").value = "Select first node";
-    injector.querySelector("#inject-many-2").onclick =
-        injectionListener(injector, 2);
+    injector.querySelector("#inject-many-2").onclick = injectionListener(injector, 2);
 }
 
 function removeManySelectionsUI() {
@@ -177,25 +198,32 @@ function addSelectionEntry() {
 }
 
 function selectionListener(entry, entryNumber) {
-    let currentLabel = entry.getElementById(`selector-xpath-label-${entryNumber}`);
+    let currentLabel = entry.getElementById(`selector-xpath-span-${entryNumber}`);
     return () => startListening(selectionHandler(currentLabel, entryNumber));
 }
 
 function selectionHandler(currentLabel, entryNumber) {
     return (mouseEvent) => {
-        let selectionValue = preExtract(mouseEvent.target, entryNumber);
+        let selectionValue = gatherValue(mouseEvent.target, entryNumber);
         addSelection(selectionValue, entryNumber);
         currentLabel.textContent = createXPathFromElement(mouseEvent.target);
     }
 }
 
 function selectionNListener(entry, entryNumber) {
-    let currentLabel = entry.querySelector(`#selector-xpath-label-${entryNumber}`);
-    return () => startListening(genericHandler(currentLabel));
+    return () => startListening(selectionNHandler(entry));
+    // return () => startListening(genericHandler(currentLabel));
+}
+
+function selectionNHandler(entry) {
+    return (mouseEvent) => {
+        entry.querySelector(".selection-value-2").value = gatherLastValue(mouseEvent.target, entry);
+        entry.querySelector(".selection-xpath-2").textContent = createXPathFromElement(mouseEvent.target);
+    }
 }
 
 function injectionListener(entry, entryNumber) {
-    let currentLabel = entry.querySelector(`#injector-xpath-label-${entryNumber}`);
+    let currentLabel = entry.querySelector(`#injector-xpath-span-${entryNumber}`);
     console.log(entry, entryNumber, currentLabel);
     return () => startListening(genericHandler(currentLabel));
 }
@@ -211,9 +239,14 @@ function genericHandler(currentLabel) {
 //     updateListener(mouseEvent);
 // }
 
-function preExtract(node, entryNumber) {
+function gatherValue(node, entryNumber) {
     console.log("swa-extraction-" + entryNumber);
     const extractionStrategy = document.getElementById("swa-extraction-" + entryNumber).value;
+    return extractionStrategies[extractionStrategy](node);
+}
+
+function gatherLastValue(node, entry) {
+    const extractionStrategy = entry.querySelector(".swa-extraction").value;
     return extractionStrategies[extractionStrategy](node);
 }
 
